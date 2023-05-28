444a76eb4759b8cfd0ff340bac58b7986662cbac
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-05-28 15:36:01 +0300

    remove phonebook App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
-// import Feedback from './Feedback/Feedback';
 import Notification from './Feedback/Notification';
 import Section from './Feedback/Section';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Statistics from './Feedback/Statistics';
-import PhoneBook from './PhoneBook/Phonebook';
 
 class App extends Component {
   static defaultProps = {
@@ -17,13 +15,6 @@ class App extends Component {
     good: this.props.goodInitialValue,
     neutral: this.props.neutralInitialValue,
     bad: this.props.badInitialValue,
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
   };
 
   onClickChange = e => {
@@ -32,34 +23,7 @@ class App extends Component {
       return { [name]: prevState[name] + 1 };
     });
   };
-  formSubmitHandler = data => {
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, data] };
-    });
-  };
-  filterHandlerData = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-  onDeleteContactHandler = data => {
-    const idToDelete = data.currentTarget.id;
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => {
-        return contact.id !== idToDelete;
-      }),
-    }));
-  };
 
-  getNormalizedContacts = () => {
-    return this.state.contacts.map(contact => {
-      return { id: contact.id, name: contact.name.toLowerCase(), number: contact.number };
-    });
-  };
-  getFilteredContacts = data => {
-    const { filter } = this.state;
-    return data.filter(contact => {
-      return contact.name.includes(filter.toLowerCase());
-    });
-  };
   countTotalFeedback() {
     const { good, neutral, bad } = this.state;
     return good + neutral + bad;
@@ -76,9 +40,6 @@ class App extends Component {
     const feedbackPercentage = this.countPositiveFeedbackPercentage();
     const { good, neutral, bad } = this.state;
     const totalFeedback = this.countTotalFeedback();
-
-    const normalizedContacts = this.getNormalizedContacts();
-    const filteredContacts = this.getFilteredContacts(normalizedContacts);
 
     return (
       <div>
@@ -98,14 +59,6 @@ class App extends Component {
             <Notification message="There is no feedback" />
           )}
         </Section>
-
-        <PhoneBook
-          onSubmitForm={this.formSubmitHandler}
-          contacts={filteredContacts}
-          filter={this.state.filter}
-          filterHandlerData={this.filterHandlerData}
-          onClickDelete={this.onDeleteContactHandler}
-        />
       </div>
     );
   }
